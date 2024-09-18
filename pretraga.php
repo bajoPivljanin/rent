bbae829c8d3b3c8d687b951c3f29d42b91b89c6d
bajoPivljanin <103775541+bajoPivljanin@users.noreply.github.com>
2024-09-18 23:37:41 +0200

    ide gas

--- pretraga.php
+++ pretraga.php
@@ -119,7 +119,7 @@
                             <div class="col-md-3">
                                 <div class="detalj-oglas">
                                 <h1>Od <br> <span>60€</span> <br>dan</h1>
-                                <a href="#">Detaljnije</a>
+                                <a href="vozilo.php">Detaljnije</a>
                                 </div>
                             </div>
                         </div>
@@ -147,7 +147,7 @@
                             <div class="col-md-3">
                                 <div class="detalj-oglas">
                                 <h1>Od <br> <span>60€</span> <br>dan</h1>
-                                <a href="#">Detaljnije</a>
+                                <a href="vozilo.php">Detaljnije</a>
                                 </div>
                             </div>
                         </div>
@@ -161,42 +161,6 @@
     </div>
 </div>
 
-<footer>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="left-footer">
-                        <a href="index.php"><img src="../public/site_img/logowhite.png" alt=""></a>
-                        <p>Brzo i jednostavno pronađi idealno vozilo za svoje putovanje, poslovnu upotrebu ili specijalnu priliku</p>
-                        <i class="fa-brands fa-instagram"></i>
-                        <i class="fa-brands fa-facebook"></i>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="mid-footer">
-                        <h4>Stranice</h4>
-                        <ul>
-                            <li><a href="#">Početna</a></li>
-                            <li><a href="#">Iznajmi</a></li>
-                            <li><a href="#">Oglašavanje</a></li>
-                            <li><a href="#">Info</a></li>
-                            <li><a href="#">Kontakt</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="right-footer">
-                        <h4>Kontakt</h4>
-                        <ul>
-                            <li><a href="#"><i class="fa-solid fa-phone"></i>079-234-123</a></li>
-                            <li><a href="#"><i class="fa-regular fa-envelope"></i>bravina@gmail.com</a></li>
-                            <li><a href="#"><i class="fa-regular fa-clock"></i>24/7</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <p id="footerp">© Copyright 2024. All rights reserved.</p>
-    </footer>
+<?php require_once "./inc/footer.php"?>
 </body>
 </html>
